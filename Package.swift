c7c879b712e58396c7c3311400a2ad5315b19504
Mx-Iris <mxiris@126.com>
2025-08-19 00:17:07 +0800

    Updates

--- Package.swift
+++ Package.swift
@@ -10,11 +10,43 @@ let package = Package(
             name: "Ccapstone",
             targets: ["Ccapstone"]
         ),
+        .executable(
+            name: "Tests",
+            targets: ["Tests"]
+        ),
     ],
     targets: [
         .target(
             name: "Ccapstone",
             path: "bindings/swift/Ccapstone",
+            cSettings: [
+                .define("CAPSTONE_HAS_ARM64"),
+                .define("CAPSTONE_HAS_ARM"),
+                .define("CAPSTONE_HAS_BPF"),
+                .define("CAPSTONE_HAS_EVM"),
+                .define("CAPSTONE_HAS_M68K"),
+                .define("CAPSTONE_HAS_M680X"),
+                .define("CAPSTONE_HAS_MIPS"),
+                .define("CAPSTONE_HAS_MOS65XX"),
+                .define("CAPSTONE_HAS_POWERPC"),
+                .define("CAPSTONE_HAS_RISCV"),
+                .define("CAPSTONE_HAS_SH"),
+                .define("CAPSTONE_HAS_SPARC"),
+                .define("CAPSTONE_HAS_SYSZ"),
+                .define("CAPSTONE_HAS_TMS320C64X"),
+//                .define("CAPSTONE_HAS_TRICORE"),
+                .define("CAPSTONE_HAS_WASM"),
+                .define("CAPSTONE_HAS_X86"),
+                .define("CAPSTONE_HAS_XCORE"),
+                .define("CAPSTONE_USE_SYS_DYN_MEM"),
+            ],
         ),
+        .executableTarget(
+            name: "Tests",
+            dependencies: [
+                "Ccapstone"
+            ],
+            path: "bindings/swift/Tests"
+        )
     ]
 )
